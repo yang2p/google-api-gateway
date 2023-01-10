5e6c8b2ec6beda5920765662bf236db7b6a5b7e6
Satoru Kobori <satoru.training.kobori@gmail.com>
2023-01-10 09:16:28 +0900

    Added 2 files

---
+++ .spectral.mjs
@@ -0,0 +1,2 @@
+import ruleset from "https://stoplight.io/api/v1/projects/cHJqOjE3NzAyMg/spectral.js?branch_slug=main&token=866c51bb-c4c8-44ff-a30d-dc642c9d14ed";
+export default { extends: ruleset };
